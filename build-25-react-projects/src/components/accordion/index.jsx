968266f6cc04e1e1adb0f3aad09405adccc2386d
nayem73 <nayem73@gmail.com>
2024-12-24 14:40:10 +0600

    Alhamdulillah Multiselect done with toggle button for single select functionality as well.

--- build-25-react-projects/src/components/accordion/index.jsx
+++ build-25-react-projects/src/components/accordion/index.jsx
@@ -4,26 +4,68 @@ import "./styles.css";
 
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
-  const handleClick = (dataItemId) => {
+  const [multiSelect, setMultiSelect] = useState([]);
+  const [isMultiSelect, setIsMultiSelect] = useState(false);
+
+  const handleSingleClick = (dataItemId) => {
     selected === dataItemId ? setSelected(null) : setSelected(dataItemId);
     console.log(dataItemId);
   };
+
+  const handleMultiClick = (dataItemId) => {
+    if (multiSelect.includes(dataItemId)) {
+      setMultiSelect(multiSelect.filter((id) => id !== dataItemId));
+    } else {
+      setMultiSelect([...multiSelect, dataItemId]);
+    }
+    console.log(multiSelect);
+  };
+
+  const toggleClick = () => {
+    setIsMultiSelect(!isMultiSelect);
+  };
+
   return (
     <div className="wrapper">
+      <button onClick={toggleClick}>
+        {isMultiSelect ? "Multi-select" : "Single-select"}
+      </button>
       {data && data.length > 0 ? (
-        data.map((dataItem) => (
-          <div key={dataItem.id}>
-            <div className="title" onClick={() => handleClick(dataItem.id)}>
-              <h3>{dataItem.question}</h3>
-              <span>+</span>
-            </div>
-            {selected === dataItem.id && (
-              <div className="content">
-                <p>{dataItem.answer}</p>
+        isMultiSelect ? (
+          data.map((dataItem) => (
+            <div key={dataItem.id}>
+              <div
+                className="title"
+                onClick={() => handleMultiClick(dataItem.id)}
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
               </div>
-            )}
-          </div>
-        ))
+              {multiSelect.includes(dataItem.id) && (
+                <div className="content">
+                  <p>{dataItem.answer}</p>
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          data.map((dataItem) => (
+            <div key={dataItem.id}>
+              <div
+                className="title"
+                onClick={() => handleSingleClick(dataItem.id)}
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id && (
+                <div className="content">
+                  <p>{dataItem.answer}</p>
+                </div>
+              )}
+            </div>
+          ))
+        )
       ) : (
         <div>No data found</div>
       )}
