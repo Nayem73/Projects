2166567e7063da68e25adfdb4dd808e0599ea344
nayem73 <nayem73@gmail.com>
2024-12-24 16:55:44 +0600

    shows color type and color with single useState

--- build-25-react-projects/src/components/random-color/index.jsx
+++ build-25-react-projects/src/components/random-color/index.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
 const RandomColor = () => {
-  const [color, setColor] = useState("Black");
+  const [color, setColor] = useState({
+    colorType: "hex",
+    color: "#f15025",
+  });
   const setTypeOfColor = (type) => {
     if (type === "hex") {
       const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
@@ -10,22 +13,33 @@ const RandomColor = () => {
         const random = Math.floor(Math.random() * hex.length);
         hexColor += hex[random];
       }
-      setColor(hexColor);
+      setColor({ colorType: "hex", color: hexColor });
     } else if (type === "rgb") {
       let rgbColor = `rgb(
       ${Math.floor(Math.random() * 256)}
       ,${Math.floor(Math.random() * 256)}
       ,${Math.floor(Math.random() * 256)}
       )`;
-      setColor(rgbColor);
+      setColor({ colorType: "rgb", color: rgbColor });
     }
   };
 
   return (
-    <div style={{ backgroundColor: color, height: "100vh", width: "100vw" }}>
-      <button onClick={() => setTypeOfColor("hex")}> Create Hex Color</button>
-      <button onClick={() => setTypeOfColor("rgb")}> Create rgb Color</button>
-    </div>
+    <>
+      <div
+        style={{
+          backgroundColor: color.color,
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <button onClick={() => setTypeOfColor("hex")}> Create Hex Color</button>
+        <button onClick={() => setTypeOfColor("rgb")}> Create rgb Color</button>
+        <h1>
+          {color.colorType} color: {color.color}
+        </h1>
+      </div>
+    </>
   );
 };
 
