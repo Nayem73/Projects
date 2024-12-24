0f9e50bc02da4f8bd435852e6849c828b65ec77c
nayem73 <nayem73@gmail.com>
2024-12-24 09:24:47 +0600

    single expand and dis exapnd is working

--- build-25-react-projects/src/components/accordion/index.jsx
+++ build-25-react-projects/src/components/accordion/index.jsx
@@ -3,13 +3,24 @@ import data from "./data";
 
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
+  const handleClick = (dataItemId) => {
+    selected === dataItemId ? setSelected(null) : setSelected(dataItemId);
+    console.log(dataItemId);
+  };
   return (
     <div>
       {data && data.length > 0 ? (
-        data.map((dataItem, index) => (
-          <div key={index}>
-            <h3>{dataItem.question}</h3>
-            {console.log(dataItem.question)}
+        data.map((dataItem) => (
+          <div key={dataItem.id}>
+            <div className="title" onClick={() => handleClick(dataItem.id)}>
+              <h3>{dataItem.question}</h3>
+              <span>+</span>
+            </div>
+            {selected === dataItem.id && (
+              <div className="content">
+                <p>{dataItem.answer}</p>
+              </div>
+            )}
           </div>
         ))
       ) : (
