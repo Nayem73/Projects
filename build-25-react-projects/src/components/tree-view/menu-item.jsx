d4fe8632bb4c9f34b6ff8dd68cc63e76c1a177fa
nayem73 <nayem73@gmail.com>
2024-12-26 14:33:18 +0600

    Used dynamic property access for toggling chilren

--- build-25-react-projects/src/components/tree-view/menu-item.jsx
+++ build-25-react-projects/src/components/tree-view/menu-item.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import { useState } from "react";
 
 const MenuItem = ({ data }) => {
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState(false);
+
+  const handleToggleChildren = (label) => {
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [label]: !displayCurrentChildren[label],
+    });
+  };
+
   return (
     <div>
       <li>
         {data.map((item, index) => (
           <div key={index}>
-            <div>label: {item.label}</div>
-            <div>to: {item.to}</div>
-            <ul>{item.children && <MenuItem data={item.children} />}</ul>
+            <div>{item.label}</div>
+            <span onClick={() => handleToggleChildren(item.label)}>+</span>
+            <ul>
+              {item && item.children && displayCurrentChildren[item.label] && (
+                <MenuItem data={item.children} />
+              )}
+            </ul>
           </div>
         ))}
       </li>
